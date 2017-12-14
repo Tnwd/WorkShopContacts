b9a1a859160d0213da2dc39b2af347588730c2e4
prasitt1122 <game12597@gmail.com>
2017-12-14 13:50:40 +0700

    if GET ID0 success show status 200

--- test/contactslist.spec.js
+++ test/contactslist.spec.js
@@ -20,3 +20,20 @@ describe('GET /contacts', () => {
             })
     })
 })
+
+describe('GET /contacts', () => {
+    it('if GET ID0 success show status 200', (done) => {
+        request(router).get('/contacts/0')
+            .expect(200)
+            .then((res) => {
+                let contact = res.body
+                expect(contact.id).toBe(0)
+                expect(contact.name).toBe('Ned Stark')
+                expect(contact.email).toBe('ned@winterfell.com')
+                expect(contact.phone).toBe('123-456-7890')
+                expect(contact.url).toBe('www.google.com')
+                expect(contact.notes).toBe('Winter is coming.')
+                done()
+            })
+    })
+})
